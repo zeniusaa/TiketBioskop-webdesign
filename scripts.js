e29a970a68c284c28c1c94431e6be2de55b01f97
Ka Yukki <tugasrakazeniusa@gmail.com>
2024-01-21 11:58:22 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("search-theater-btn").addEventListener("click", function() {
+    window.open("https://21cineplex.com/theaters", "_blank");
+});
+
+document.getElementById("info-btn").addEventListener("click", function() {
+    window.open("https://21cineplex.com/info", "_blank");
+});
